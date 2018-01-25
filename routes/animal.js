aa400e97ffcf3eb2de2b7911b8a901afa060fcc5
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-25 16:36:24 +0100

    retour payload animal

--- routes/animal.js
+++ routes/animal.js
@@ -6,12 +6,22 @@ router.get('/', (req, res) => {
   res.send("animal is comming");
 });
 
+
+
+
+
+
 // POST method
 router.post('/', (req, res) => {
   const payload = req.body;
   console.log("/animal payload received: " + JSON.stringify(payload)); // in heroku logs
 
-  res.send("Animal received");
+  let animal = req.body;
+  animal.valide = true;
+
+
+  res.send(JSON.stringify(animal));
+
 
 });
 
