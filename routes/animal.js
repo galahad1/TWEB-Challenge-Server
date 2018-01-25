9b47ae83f3b8372ce6b26a927941fad8b33ff40f
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-25 16:31:56 +0100

    ajout handler post /animals

--- routes/animal.js
+++ routes/animal.js
@@ -6,4 +6,15 @@ router.get('/', (req, res) => {
   res.send("animal is comming");
 });
 
+// POST method
+router.post('/', (req, res) => {
+  const payload = req.body;
+  console.log("/animal payload received: " + JSON.stringify(payload)); // in heroku logs
+
+  res.send("Animal received");
+
+});
+
+
+
 module.exports = router;
