538ca8ea3ad99191e541c6f1cce87914fb09f834
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-25 21:50:47 +0100

    eslint frappa

--- routes/chance.js
+++ routes/chance.js
@@ -1,14 +1,14 @@
+/* eslint-disable one-var */
 const express = require('express');
-const router = express.Router();
 
-var Chance = require('chance'),
+const router = express.Router();
+const Chance = require('chance'),
   chance = new Chance();
 
 /* GET chance page. */
 router.get('/', (req, res) => {
-res.send(chance.animal());
+  res.send(chance.animal());
 });
-
 
 
 module.exports = router;
