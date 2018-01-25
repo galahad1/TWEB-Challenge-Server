a178e1c7e10c7f79262afb5c031a3adccfd7eab3
Tano Iannetta <tano.iannetta@heig-vd.ch>
2018-01-25 21:49:33 +0100

    check code ajout eslint

--- routes/animal.js
+++ routes/animal.js
@@ -1,31 +1,25 @@
+/* eslint-disable no-template-curly-in-string */
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.send("animal is comming");
+  res.send('animal is comming');
 });
-
-
-
-
 
 
 // POST method
 router.post('/', (req, res) => {
   const payload = req.body;
-  console.log("/animal payload received: " + JSON.stringify(payload)); // in heroku logs
+  console.log('/animal payload received: ${JSON.stringify(payload)}'); // in heroku logs
 
-  let animal = req.body;
+  const animal = payload;
   animal.valide = true;
 
 
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(animal));
-
-
 });
-
-
 
 module.exports = router;
